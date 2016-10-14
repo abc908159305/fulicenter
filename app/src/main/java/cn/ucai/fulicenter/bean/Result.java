99c134f5c8d1b60295befbb974476db97f5ebf12
abc908159305 <abc2052yang@outlook.com>
2016-10-14 15:28:17 +0800

    重新增加12个实体类

---
+++ app/src/main/java/cn/ucai/fulicenter/bean/Result.java
@@ -0,0 +1,56 @@
+package cn.ucai.fulicenter.bean;
+
+/**
+ * Created by Administrator on 2016/10/13.
+ */
+public class Result {
+
+    /**
+     * retCode : 0
+     * retMsg : true
+     * retData : null
+     */
+
+    private int retCode;
+    private boolean retMsg;
+    private UserAvatarBean retData;
+
+    public Result(int retCode, boolean retMsg, UserAvatarBean retData) {
+        this.retCode = retCode;
+        this.retMsg = retMsg;
+        this.retData = retData;
+    }
+
+    public int getRetCode() {
+        return retCode;
+    }
+
+    public void setRetCode(int retCode) {
+        this.retCode = retCode;
+    }
+
+    public boolean isRetMsg() {
+        return retMsg;
+    }
+
+    public void setRetMsg(boolean retMsg) {
+        this.retMsg = retMsg;
+    }
+
+    public UserAvatarBean getRetData() {
+        return retData;
+    }
+
+    public void setRetData(UserAvatarBean retData) {
+        this.retData = retData;
+    }
+
+    @Override
+    public String toString() {
+        return "Result{" +
+                "retCode=" + retCode +
+                ", retMsg=" + retMsg +
+                ", retData=" + retData +
+                '}';
+    }
+}
