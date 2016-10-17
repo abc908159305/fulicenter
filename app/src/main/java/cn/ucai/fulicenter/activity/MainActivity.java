aa182f3a6f2b5e99388f1a8757e256422802108d
abc908159305 <abc2052yang@outlook.com>
2016-10-17 10:33:05 +0800

    添加了ButterKnife、为底部菜单栏新增互斥

--- app/src/main/java/cn/ucai/fulicenter/activity/MainActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/MainActivity.java
@@ -1,19 +1,74 @@
 package cn.ucai.fulicenter.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.RadioButton;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import cn.ucai.fulicenter.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    @Bind(R.id.layout_new_good)
+    RadioButton layoutNewGood;
+    @Bind(R.id.layout_boutique)
+    RadioButton layoutBoutique;
+    @Bind(R.id.layout_category)
+    RadioButton layoutCategory;
+    @Bind(R.id.layout_cart)
+    RadioButton layoutCart;
+    @Bind(R.id.layout_personal_center)
+    RadioButton layoutPersonalCenter;
+
+    int index;
+    RadioButton rbs[];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        initrbs();
     }
-    public void onCheckedChange(View view){
 
+    private void initrbs() {
+        rbs = new RadioButton[5];
+        rbs[0] = layoutNewGood;
+        rbs[1] = layoutBoutique;
+        rbs[2] = layoutCategory;
+        rbs[3] = layoutCart;
+        rbs[4] = layoutPersonalCenter;
+    }
+
+    public void onCheckedChange(View view) {
+        switch (view.getId()) {
+            case R.id.layout_new_good:
+                index = 0;
+                break;
+            case R.id.layout_boutique:
+                index = 1;
+                break;
+            case R.id.layout_category:
+                index = 2;
+                break;
+            case R.id.layout_cart:
+                index = 3;
+                break;
+            case R.id.layout_personal_center:
+                index = 4;
+                break;
+        }
+        setRadioButton();
+    }
+
+    private void setRadioButton() {
+        for (int i = 0;i<rbs.length;i++) {
+            if (i == index) {
+                rbs[i].setChecked(true);
+            } else {
+                rbs[i].setChecked(false);
+            }
+        }
     }
 }
