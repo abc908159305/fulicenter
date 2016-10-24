3ccc36832a4fce5c2c8541fdcdfc5a7aaf3bbcea
abc908159305 <abc2052yang@outlook.com>
2016-10-24 11:11:35 +0800

    修复bug：DBOpenHelper中sql语句没有空格

--- app/src/main/java/cn/ucai/fulicenter/dao/DBOpenHelper.java
+++ app/src/main/java/cn/ucai/fulicenter/dao/DBOpenHelper.java
@@ -12,12 +12,12 @@ import cn.ucai.fulicenter.I;
 public class DBOpenHelper extends SQLiteOpenHelper{
     private static final int DATABASE_VERSION = 1;
     private static DBOpenHelper instance;
-    private static final String FULICENTER_USER_TABLE_CREATE = "CREATE TABLE"
-            + UserDao.USER_TABLE_NAME + "("
-            + UserDao.USER_COLUMN_NAME + " TEXT PRIMARY KEY,"
+    private static final String FULICENTER_USER_TABLE_CREATE = "CREATE TABLE "
+            + UserDao.USER_TABLE_NAME + " ("
+            + UserDao.USER_COLUMN_NAME + " TEXT PRIMARY KEY, "
             + UserDao.USER_COLUMN_NICK + " TEXT, "
             + UserDao.USER_COLUMN_AVATAR_ID + " INTEGER, "
-            + UserDao.USER_COLUMN_AVATAR_TYPE + " TEXT,"
+            + UserDao.USER_COLUMN_AVATAR_TYPE + " TEXT, "
             + UserDao.USER_COLUMN_AVATAR_PATH + " TEXT, "
             + UserDao.USER_COLUMN_AVATAR_SUFFIX + " TEXT, "
             + UserDao.USER_COLUMN_AVATAR_LASTUDATE_TIME + " TEXT);";
