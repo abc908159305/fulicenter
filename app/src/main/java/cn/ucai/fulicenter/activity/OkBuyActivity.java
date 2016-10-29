e1261a96857091e7b1edbd0bc4cadb082d30bca8
abc908159305 <abc2052yang@outlook.com>
2016-10-29 14:36:41 +0800

    添加混淆，并增加支付完成删除购物车商品的功能

--- app/src/main/java/cn/ucai/fulicenter/activity/OkBuyActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/OkBuyActivity.java
@@ -26,6 +26,7 @@ import cn.ucai.fulicenter.FuLiCenterApplication;
 import cn.ucai.fulicenter.I;
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.bean.CartBean;
+import cn.ucai.fulicenter.bean.MessageBean;
 import cn.ucai.fulicenter.bean.User;
 import cn.ucai.fulicenter.net.NetDao;
 import cn.ucai.fulicenter.net.OkHttpUtils;
@@ -220,6 +221,34 @@ public class OkBuyActivity extends BaseActivity implements PaymentHandler{
                     e.printStackTrace();
                 }
             }
+            int resultCode = data.getExtras().getInt("code");
+            switch (resultCode) {
+                case 1:
+                    paySuccess();
+                    CommonUtils.showLongToast("付款成功");
+                    break;
+                case -1:
+                    CommonUtils.showLongToast("付款失败");
+                    finish();
+                    break;
+            }
         }
+    }
+
+    private void paySuccess() {
+        for (String id : ids) {
+            NetDao.deleteCart(mContext, Integer.valueOf(id), new OkHttpUtils.OnCompleteListener<MessageBean>() {
+                @Override
+                public void onSuccess(MessageBean result) {
+
+                }
+
+                @Override
+                public void onError(String error) {
+
+                }
+            });
+        }
+        finish();
     }
 }
