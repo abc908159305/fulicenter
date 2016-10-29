480fa1bb08fa704a5daf5adb1ca3ebfcdfd5ebb1
abc908159305 <abc2052yang@outlook.com>
2016-10-29 14:12:01 +0800

    导入壹收款支付，并在支付页面实现该功能

--- app/src/main/java/cn/ucai/fulicenter/activity/OkBuyActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/OkBuyActivity.java
@@ -1,12 +1,23 @@
 package cn.ucai.fulicenter.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.pingplusplus.android.PingppLog;
+import com.pingplusplus.libone.PaymentHandler;
+import com.pingplusplus.libone.PingppOne;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -22,7 +33,7 @@ import cn.ucai.fulicenter.utils.CommonUtils;
 import cn.ucai.fulicenter.utils.ResultUtils;
 import cn.ucai.fulicenter.view.DisplayUtils;
 
-public class OkBuyActivity extends BaseActivity {
+public class OkBuyActivity extends BaseActivity implements PaymentHandler{
 
     @Bind(R.id.et_Name)
     EditText metName;
@@ -41,6 +52,8 @@ public class OkBuyActivity extends BaseActivity {
     ArrayList<CartBean> mList = null;
     String[] ids = new String[]{};
     int rankPrice = 0;
+    private static String URL = "http://218.244.151.190/demo/charge";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_ok_buy);
@@ -48,6 +61,13 @@ public class OkBuyActivity extends BaseActivity {
         mContext = this;
         mList = new ArrayList<>();
         super.onCreate(savedInstanceState);
+        //设置需要使用的支付方式
+        PingppOne.enableChannels(new String[]{"wx", "alipay", "upacp", "bfb", "jdpay_wap"});
+        // 提交数据的格式，默认格式为json
+        // PingppOne.CONTENT_TYPE = "application/x-www-form-urlencoded";
+        PingppOne.CONTENT_TYPE = "application/json";
+
+        PingppLog.DEBUG = true;
     }
 
     @Override
@@ -85,7 +105,7 @@ public class OkBuyActivity extends BaseActivity {
             metNumber.requestFocus();
             return;
         }
-        if (!number.matches("[\\d]{11}]")) {
+        if (!number.matches("[\\d]{11}")) {
             metNumber.setError("手机号码格式错误");
             metNumber.requestFocus();
             return;
@@ -104,7 +124,32 @@ public class OkBuyActivity extends BaseActivity {
         gotoStatements();
     }
     private void gotoStatements() {
+        // 产生个订单号
+        String orderNo = new SimpleDateFormat("yyyyMMddhhmmss")
+                .format(new Date());
 
+        // 构建账单json对象
+        JSONObject bill = new JSONObject();
+
+        // 自定义的额外信息 选填
+        JSONObject extras = new JSONObject();
+        try {
+            extras.put("extra1", "extra1");
+            extras.put("extra2", "extra2");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            bill.put("order_no", orderNo);
+            bill.put("amount", rankPrice*100);
+            bill.put("extras", extras);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        //壹收款: 创建支付通道的对话框
+        PingppOne.showPaymentChannels(getSupportFragmentManager(), bill.toString(), URL, this);
     }
 
 
@@ -145,5 +190,36 @@ public class OkBuyActivity extends BaseActivity {
     private int getPrice(String price) {
         price = price.substring(price.indexOf("￥") + 1);
         return Integer.valueOf(price);
+    }
+
+    @Override
+    public void handlePaymentResult(Intent data) {
+        if (data != null) {
+
+            // result：支付结果信息
+            // code：支付结果码
+            //-2:用户自定义错误
+            //-1：失败
+            // 0：取消
+            // 1：成功
+            // 2:应用内快捷支付支付结果
+
+            if (data.getExtras().getInt("code") != 2) {
+                PingppLog.d(data.getExtras().getString("result") + "  " + data.getExtras().getInt("code"));
+            } else {
+                String result = data.getStringExtra("result");
+                try {
+                    JSONObject resultJson = new JSONObject(result);
+                    if (resultJson.has("error")) {
+                        result = resultJson.optJSONObject("error").toString();
+                    } else if (resultJson.has("success")) {
+                        result = resultJson.optJSONObject("success").toString();
+                    }
+                    PingppLog.d("result::" + result);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
